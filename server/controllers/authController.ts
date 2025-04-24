75c9dbd4f75bdcfc7065ee0b6efa37c61c31336e
Sujan299 <mrsujan321@gmail.com>
2025-04-24 18:20:55 +0545

    second commit

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,6 +1,46 @@
-import express, {Request, Response, NextFunction} from 'express';
-
-
-const signup = (req: Request, res: Response, next: NextFunction)=>{
-    
+import {Request, Response, NextFunction} from 'express';
+import bcrypt from 'bcryptjs';
+import prisma from '../prisma/client';
+interface SignupData {
+    username: string;
+    email: string;
+    password: number;
+    dob: Date;
+}
+export const signup = async (req: Request<{}, {}, SignupData >, res: Response, next: NextFunction)=>{
+    const {username,email, password, dob} = req.body;
+    const existingUser = await prisma.user.findUnique({where : {email}});
+    if(existingUser){
+        return res.status(401).json({message: "User already exist!"})
+    }
+    // if email is already exist then return from here directly.
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+        data:{
+            username,
+            email,
+            password: hashPassword,
+            dob
+        }
+    })
+    res.status(201).json({message: "User created successfully" + newUser.id})
+}
+interface loginBody{
+    email: string;
+    password: string;
+}
+export const login = async (req: Request<{},{},loginBody>, res: Response)=>{
+    const {email, password} = req.body;
+    // if email not matched, return from here with some status
+    const existingUser = await prisma.user.findUnique({where: {email}});
+    if(!existingUser){
+        return res.status(401).json({message: "User does not exist !"})
+    }
+    const hashPassword = existingUser.password;
+    const isMatch = await bcrypt.compare(password, hashPassword);
+    if(isMatch){
+        res.status(200).send({meassge: "Login successfully !"})
+    }else{
+        res.status(401).send({message: "can not login "})
+    }
 }
--- server/routes/authRoutes.ts
+++ server/routes/authRoutes.ts
@@ -1,9 +1,14 @@
 import express,{Request, Response, NextFunction} from 'express';
 const router = express.Router();
+import {signup, login} from '../controllers/authController'
+
 
 router.get("/", (req:Request, res:Response, next:NextFunction)=>{
     res.send("I am from router !");
 })
+
+router.post("/login", login)
+router.post("/signup", signup)
 
 
 module.exports = router;
